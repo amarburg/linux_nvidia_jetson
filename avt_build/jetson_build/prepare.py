cc303a4b5bddb3ca437d0e3711a6abe0fbb4f1b5
Aaron Marburg <amarburg@uw.edu>
2024-10-02 15:59:17 -0700

    Update version numbers and minor process improvements.

--- avt_build/jetson_build/prepare.py
+++ avt_build/jetson_build/prepare.py
@@ -2,6 +2,7 @@ import logging
 import shutil
 from . import tools
 from . import common
+from pathlib import Path
 
 def clean(args, board):
   logging.info(f"Cleaning {board.name} in {board.build_dir}")
@@ -12,10 +13,24 @@ def clean(args, board):
 def prepare(args, board):
   t = tools.tools(args)
   logging.info(f"Preparing {board.name} in {board.build_dir}")
-  logging.info("Extracting driver package")
-  t.extract(board.files.driver_package, board.build_dir, sudo=True)
-  #logging.info("Extracting rootfs")
-  #t.extract(board.files.rootfs, board.build_dir / 'Linux_for_Tegra/rootfs', sudo=True)
+
+  l4t_path = Path(board.build_dir) / "Linux_for_Tegra"
+
+  if l4t_path.exists():
+    logging.info(f"L4T path {l4t_path} exists, not extracting")
+  else:
+    logging.info("Extracting driver package")
+    t.extract(board.files.driver_package, board.build_dir, sudo=True)
+
+  rootfs_path = l4t_path / "rootfs"
+
+  # Arbitrary choice of a directory which exists in the rootfs tarball
+  if (rootfs_path / "lib").exists():
+    logging.info(f"Rootfs {rootfs_path} exists, not extracting")
+  else:
+    logging.info("Extracting rootfs")
+    t.extract(board.files.rootfs, str(rootfs_path), sudo=True)
+
   logging.warning("Extracting public_sources DISABLED")
   #t.extract(board.files.public_sources, board.build_dir)
   #t.execute(['sudo', './apply_binaries.sh'], cwd=board.build_dir / 'Linux_for_Tegra')
