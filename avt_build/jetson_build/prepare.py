85da541a76f498454d6b8984d678ea339e38c3a7
Aaron Marburg <amarburg@uw.edu>
2024-10-07 13:18:12 -0700

    Check if BSP exists before unpacking redundantly

--- avt_build/jetson_build/prepare.py
+++ avt_build/jetson_build/prepare.py
@@ -2,6 +2,7 @@ import logging
 import shutil
 from . import tools
 from . import common
+from pathlib import Path
 
 def clean(args, board):
   logging.info(f"Cleaning {board.name} in {board.build_dir}")
@@ -12,8 +13,13 @@ def clean(args, board):
 def prepare(args, board):
   t = tools.tools(args)
   logging.info(f"Preparing {board.name} in {board.build_dir}")
-  logging.info("Extracting driver package")
-  t.extract(board.files.driver_package, board.build_dir, sudo=True)
+
+  if (Path(board.build_dir) / "Linux_for_Tegra").exists():
+    logging.info(f"Driver package exists, not extracting")
+  else:
+    logging.info("Extracting driver package")
+    t.extract(board.files.driver_package, board.build_dir, sudo=True)
+
   #logging.info("Extracting rootfs")
   #t.extract(board.files.rootfs, board.build_dir / 'Linux_for_Tegra/rootfs', sudo=True)
   logging.warning("Extracting public_sources DISABLED")
